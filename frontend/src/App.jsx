8e31d4fc8c18592c8c914175db0d5d9f541fc8ee
samadhan shinde <samadhanshinde9850@gmail.com>
2024-10-22 21:55:08 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,7 @@ function capitalizeFirstLetter(string) {
 }
 
 const App = () => {
-  const [query, setQuery] = useState({ q: "ahmedabad" });
+  const [query, setQuery] = useState({ q: "pune" });
   const [units, setUnits] = useState("metric");
   const [weather, setWeather] = useState(null);
   const [threshold,setThreshold]=useState(35);
